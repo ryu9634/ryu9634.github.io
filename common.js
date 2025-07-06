4f6d5bc8f79717721305bedcc1e61513472ea250
ryu <ryusp0515@naver.com>
2025-07-06 14:56:21 +0900

    수정중

---
+++ common.js
@@ -0,0 +1,18 @@
+const portfolio = [
+    ['국립중앙도서관 통계자료시스템 통합유지관리',
+     ''
+    ],
+    ['국립중앙도서관 대표 누리집 통합유지관리',''],
+    ['국립중앙도서관 국가 전자도서관 통합유지관리',''],
+    ['국립어린이 청소년 도서관 통합유지관리','']
+];
+
+
+
+ function openPopup() {
+    document.getElementById("popupLayer").style.display = "flex";
+  }
+
+  function closePopup() {
+    document.getElementById("popupLayer").style.display = "none";
+  }
